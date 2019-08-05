4a41b2175db5ef1174a4bbdd89b18a64a8d354a2
mohit423 <38151935+mohit423@users.noreply.github.com>
2019-08-06 00:08:09 +0530

    Update BankProjectApplicationTests.java

--- src/test/java/com/example/demo/BankProjectApplicationTests.java
+++ src/test/java/com/example/demo/BankProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+/*package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,4 +13,4 @@ public class BankProjectApplicationTests {
 	public void contextLoads() {
 	}
 
-}
+}*/
